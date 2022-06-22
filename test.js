4af10d95a824aa008f5ca7a05cf1abaa89be77f7
DKalaytanov <kalaytanov@gmail.com>
2022-06-22 09:40:19 +0300

    feat: add social links and destinations foto

---
+++ test.js
@@ -0,0 +1,59 @@
+// let string = 'srt';
+// const number = 10;
+// const number2 = 15;
+// const boolean = true;
+// const boolean1 = false;
+
+// console.log(Boolean(number));
+
+// for(let i = 10; i <= 20; i+=0.5) {
+//     console.log(i);
+// };
+
+// let i = (a / b === 0);
+
+// do {
+//     console.log(i);
+//     i++
+    
+// while(i<=10);
+
+// while(i<=10) {
+//     console.log(i);
+//     i++
+// }
+
+// const a = 10;
+// const b = 5;
+
+// if (i = (a % b === 0)) {
+//     console.log(i);
+// } else {
+//     console.log('true');
+// }
+
+// function addNum (number, number2) {
+//     if(!(!number||!number2)) {
+//         return number + number2;
+//     } else {
+//         return number*number2;
+//     }
+// };
+
+// const nam = addNum(20, 30);
+// console.log(nam);
+
+// let name = 'Vasia'; 
+// name[3] = 'y'; 
+// let name = 12
+// const greeting = 'Hello ' + name
+
+// let name = 'Vasia'; 
+// name.length = 50;
+
+// a = true, b = true
+
+
+const a = ''; const b = 9; const c = null;
+console.log(!(a || !b && !c))
+
